a12041711e8b7b5624aca954eac707c0474dd91b
coryselzer <134936973+coryselzer@users.noreply.github.com>
2023-08-15 14:01:17 -0500

    Delete DataAnalysis2.sql
    Combined all analysis into one document

--- EmployeeSQL/DataAnalysis2.sql
+++
@@ -1,4 +0,0 @@
--- List first name, last name, and hire date for employees hired in 1986
-select first_name, last_name, hire_date
-from employees
-where hire_date between '1986-01-01' and '1986-12-31';
