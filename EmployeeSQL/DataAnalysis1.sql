ac0cb0f20a8cd70660a9aa46130c0f459059a93d
coryselzer <134936973+coryselzer@users.noreply.github.com>
2023-08-15 14:00:49 -0500

    Delete DataAnalysis1.sql
    Combined all analysis into one document

--- EmployeeSQL/DataAnalysis1.sql
+++
@@ -1,5 +0,0 @@
--- List employee number, last name, first, name, sex, and salary of each employee
-select e.emp_no, last_name, first_name, sex, salary
-from employees e
-join salaries s
-	on e.emp_no = s.emp_no;
